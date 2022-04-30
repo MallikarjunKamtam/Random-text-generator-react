f9cd6eac6307eefcdea5de82c53647dbf649cc71
Mallikarjun <mallikarjunkamtam@gmail.com>
2022-04-30 21:08:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+import data from "./data";
 
 function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let size = +count;
+
+    if (count <= 0) {
+      size = 0;
+    }
+    if (count >= 16) {
+      size = 16;
+    }
+
+    setText(data.slice(0, size));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>RANDOM TEXT GENERATOR</h1>
+        <h4>Select between 1 to 16</h4>
+        <form action="#">
+          <label className="label" htmlFor="size">
+            Enter the Size:{" "}
+          </label>
+          <input
+            value={count}
+            id="size"
+            onChange={(e) => setCount(e.target.value)}
+            type="number"
+            name="size"
+          />
+          <button className="btn" type="submit" onClick={submitHandler}>
+            SUBMIT
+          </button>
+        </form>
+
+        {/* <p>{text[count]}</p> */}
+
+        <div className="text-area">
+          {text.map((x, i) => {
+            return <p key={i}>{x}</p>;
+          })}
+        </div>
+      </section>
     </div>
   );
 }
